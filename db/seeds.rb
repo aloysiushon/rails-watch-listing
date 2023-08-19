82420fe0b5a7b4c6c50dfb89c9253d1006f97cf9
Hon Aloysius <aloysiushon@hotmail.com>
2023-08-19 20:51:27 +0800

    Added Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning up database...'
+Moviee.destroy_all
+puts 'Database cleaned'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+20.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  moviees = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  moviees.each do |moviee|
+    puts "Creating #{moviee['title']}"
+    base_poster_url = 'https://image.tmdb.org/t/p/original'
+    Moviee.create(
+      title: moviee['title'],
+      overview: moviee['overview'],
+      poster_url: "#{base_poster_url}#{moviee['backdrop_path']}",
+      rating: moviee['vote_average']
+    )
+  end
+end
+puts 'Movies created'
